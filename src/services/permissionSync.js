7b9285769ff206a89c9b88b60356cc367ba8deca
ilumaaventures <ilumaaventures@gmail.com>
2026-05-18 15:36:29 +0530

    feat: Add legacy permission mapping for candidate resume download

--- src/services/permissionSync.js
+++ src/services/permissionSync.js
@@ -10,6 +10,7 @@ const LEGACY_PERMISSION_REPLACEMENTS = {
     'ta.application.manage.all': 'ta.candidate.manage.all',
     'ta.candidate.view.client_name': 'ta.client.confidential.view',
     'ta.candidate.view.pii': 'ta.candidate.sensitive.view',
+    'ta.candidate.resume.download': 'ta.resume.download',
     'ta.candidate.evaluate_round': 'ta.interview.evaluate',
     'ta.config.manage': 'ta.config.edit'
 };
