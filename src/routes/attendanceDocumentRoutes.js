3b217034cff53f1dabb726ab0f2b49db007f558c
ilumaaventures <ilumaaventures@gmail.com>
2026-06-22 12:06:23 +0530

    feat: implement attendance document routes and integrate Cloudinary storage middleware

--- src/routes/attendanceDocumentRoutes.js
+++ src/routes/attendanceDocumentRoutes.js
@@ -2,16 +2,25 @@ const express = require('express');
 const router = express.Router();
 const attachmentController = require('../controllers/attendanceDocumentController');
 const { protect } = require('../middlewares/authMiddleware');
-const { upload } = require('../config/cloudinary');
+const { uploadAttendanceDocuments } = require('../config/cloudinary');
+
+const uploadMiddleware = (req, res, next) => {
+    uploadAttendanceDocuments.single('file')(req, res, (err) => {
+        if (err) {
+            return res.status(400).json({ message: err.message });
+        }
+        next();
+    });
+};
 
 router.use(protect);
 
 router.get('/:userId/:month', attachmentController.getAttachments);
-router.post('/:userId/:month', upload.single('file'), attachmentController.uploadAttachment);
+router.post('/:userId/:month', uploadMiddleware, attachmentController.uploadAttachment);
 router.delete('/:userId/:month/:fileId', attachmentController.deleteAttachment);
 
 router.put('/:userId/:month/:fileId/submit', attachmentController.submitAttachmentForApproval);
 router.put('/:userId/:month/:fileId/review', attachmentController.reviewAttachment);
-router.put('/:userId/:month/:fileId/replace', upload.single('file'), attachmentController.replaceAttachment);
+router.put('/:userId/:month/:fileId/replace', uploadMiddleware, attachmentController.replaceAttachment);
 
 module.exports = router;
